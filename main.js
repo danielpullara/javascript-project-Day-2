afda8109f7af3f96588f38baab179367e3a587d6
dannyp <danielpullara92@gmail.com>
2020-03-03 17:37:30 +0700

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const currency = prompt("Please Input Currency VND | USD");
-const desiredCurrency = prompt("enter your desired currency")
+const currency = "USD"
+const desiredCurrency = "VND"
 const option = currency.toUpperCase() + "-" + desiredCurrency.toUpperCase()
 
 const exchangeRate = 23208; // USD to VND & VND to USD// 
@@ -7,128 +7,112 @@ const krwtousd = 1194; // KRW to USD & //
 const eurtousd =  0.901075; //EUR to USD//
 const inrtousd =  72.7817; // INR to USD //
 const krwtoVnd =  19.4844; // KRW to VND //
-const eurtoVnd = 25825.29 // EUR to VND //
-const inrtoVnd = 319.346 // INR to VND //
+const eurtoVnd = 25825.29; // EUR to VND //
+const inrtoVnd = 319.346; // INR to VND //
+const usdtoUsd =  0.901075; //EUR to USD//
 
+let amountInput = document.getElementById ('amount');
+let resultArea =  document.getElementById ('result-area');
+let from = document.getElementById("from");
+let to = document.getElementById("to");
 
+function convertToVnd() { 
+    const amount = amountInput.value
+    const option = from.value.toUpperCase()+"-"+to.value.toUpperCase()
+    if(amount > 0 ){
+        switch (option){
+            case "INR-USD" :
+                resultArea.innerHTML= inrToUsd(amount)
+                break;
+            case "EUR-USD" :
+                resultArea.innerHTML=eurToUsd(amount)
+                break;
+            case "KRW-USD" :
+                resultArea.innerHTML=krwToUsd(amount)
+                break;
+            case "VND-USD" :
+                resultArea.innerHTML=vndToUsd(amount)
+                break;
+            case "USD-VND":
+                resultArea.innerHTML=usdToVnd(amount)
+                break;
+            case "KRW-VND":
+                resultArea.innerHTML=krwToVnd(amount)
+                break;
+            case "EUR-VND":
+                resultArea.innerHTML= eurToVnd(amount)
+                break;
+            case "INR-VND":
+                resultArea.innerHTML=inrToVnd(amount)
+                break;
+            case "USD-EUR":
+                resultArea.innerHTML=usdtoEur(amount)
+                break;               
+            default : 
+            console.log("plsease enter a correct currency")
+        }
+    } else {
+        console.log("please input a valid number")
+    }
+}
 
-function eurToUsd () {
+function eurToUsd (amount) {
     const convertedAmount = (amount / eurtousd).toFixed(2)
     const a = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(convertedAmount)
     
-    console.log(amount, "EUR equals to", a)
+    return amount +" EUR equals to "+a
     }
 
-function inrToVnd() {
+function inrToVnd(amount) {
     const convertedAmount = (amount * inrtoVnd).toFixed(2)
     const a = new Intl.NumberFormat('en-VN', { style: 'currency', currency: 'VND' }).format(convertedAmount)
-    console.log(amount, "USD equals to", a)
+    return amount+" EUR equals to "+a
 }
 
-function eurToVnd() {
+function eurToVnd(amount) {
     const convertedAmount = (amount * eurtoVnd).toFixed(2)
     const a = new Intl.NumberFormat('en-VN', { style: 'currency', currency: 'VND' }).format(convertedAmount)
-    console.log(amount, "USD equals to", a)
+    return amount+" EUR equals to "+a
 }
 
-function krwToVnd() {
+function krwToVnd(amount) {
     const convertedAmount = (amount * krwtoVnd).toFixed(2)
     const a = new Intl.NumberFormat('en-VN', { style: 'currency', currency: 'VND' }).format(convertedAmount)
-    console.log(amount, "USD equals to", a)
+    return amount+" KRW equals to "+a
 }
 
-function usdToVnd() {
+function usdToVnd(amount) {
     const convertedAmount = (amount * exchangeRate).toFixed(2)
     const a = new Intl.NumberFormat('en-VN', { style: 'currency', currency: 'VND' }).format(convertedAmount)
-    console.log(amount, "USD equals to", a)
+    resultArea.innerHTML = `${amount}, USD equals to ${a}`
+    // return amount+" EUR equals to "+a
 }
 
-function inrToUsd () {
+function inrToUsd (amount) {
     const convertedAmount = (amount / inrtousd).toFixed(2)
     const a = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(convertedAmount)
     
-    console.log(amount, "INR equals to", a)
+    return amount+" EUR equals to "+a
     }
 
-function eurToUsd () {
+function eurToUsd (amount) {
     const convertedAmount = (amount * eurtousd).toFixed(2)
     const a = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(convertedAmount)
     
-    console.log(amount, "EUR equals to", a)
+    return amount+" EUR equals to "+a
     }
 
-function krwToUsd () {
-const convertedAmount = (amount / krw2usd).toFixed(2)
+function krwToUsd (amount) {
+const convertedAmount = (amount / krwtousd).toFixed(2)
 const a = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(convertedAmount)
 
-console.log(amount, "KRW equals to", a)
+return amount+" KRW equals to "+a
 }
-function vndToUsd() {
+function vndToUsd(amount) {
     const convertedAmount = (amount / exchangeRate).toFixed(2)
     const a = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(convertedAmount)
 
-    console.log(amount, "VND equals to", a)
+    return amount+" EUR equals to "+a
 }
 
-
-
-// if (currency.toUpperCase() === "VND") {
-//     amount = prompt("How much is that");
-//     if (amount > 0) {
-//         vndToUsd()
-//     }
-//     else {
-//         console.log("Please input a valid number")
-//     }
-// }
-// else if (currency.toUpperCase() === "USD") {
-//     amount = prompt("How much is that?");
-//     if (amount > 0) {
-//         usdToVnd();
-//     }
-//     else {
-//         console.log("please input a valid number")
-//     }
-
-// }
-// else {
-//     console.log("input a correct currency")
-// }
-
-let amount = prompt("amount")
-if(amount > 0 ){
-    switch (option){
-        case "INR-USD" :
-            inrToUsd()
-            break;
-        case "EUR-USD" :
-            eurToUsd()
-            break;
-        case "KRW-USD" :
-            krwToUsd()
-            break;
-        case "VND-USD" :
-            vndToUsd()
-            break;
-        case "USD-VND":
-            usdToVnd()
-            break;
-        case "KRW-VND":
-            krwToVnd()
-            break;
-        case "EUR-VND":
-            eurToVnd()
-            break;
-        case "INR-VND":
-            inrToVnd()
-            break;
-        case "USD-EUR":
-            usdtoEur()
-            break;               
-        default : 
-        console.log("plsease enter a correct currency")
-    }
-} else {
-    console.log("please input a valid number")
-}
 
